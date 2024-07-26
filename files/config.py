d3f6862f49380a6f6de14b4fa8f0fcfb5af69cb1
Ubuntu <ubuntu@ip-172-31-13-32.ap-south-1.compute.internal>
2024-07-26 12:25:49 +0000

    update all changing

--- files/config.py
+++ files/config.py
@@ -1,5 +1,9 @@
-import os
-
-SECRET_KEY = '{{ secret_key }}'
-DB_PATH = '{{ db_path }}'
+# files/config.py
+PORT = {{ port }}
+DEPLOYMENT_PATH = "{{ deployment_path }}"
+WHEEL_FILE = "{{ wheel_file }}"
+INSTANCE_PATH = "{{ instance_path }}"
+SECRET_KEY = "{{ secret_key }}"
+DB_PATH = "{{ db_path }}"
 ADMIN_GROUPS = {{ admin_groups | tojson }}
+
--- files/run.sh
+++ files/run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-DEPLOYMENT_FOLDER='<deployment_folder>'
-
+# Ensure the deployment folder variable is set
+DEPLOYMENT_FOLDER="/opt/example"
+PORT="5000"
+# Activate the virtual environment
 source $DEPLOYMENT_FOLDER/venv/bin/activate
 
+# Start the application using gunicorn
 gunicorn app:application \
-         --bind 127.0.0.1:<port> \
+         --bind 0.0.0.0:$PORT \
          --workers 2 \
          --threads 2
+
+
